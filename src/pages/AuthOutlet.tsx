31b284d963f8ff454d554fa0f9eb7aa96901a3cd
dsootII <155128636+dsootII@users.noreply.github.com>
2024-07-08 22:44:07 +0530

    Update AuthOutlet.tsx

--- src/pages/AuthOutlet.tsx
+++ src/pages/AuthOutlet.tsx
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../lib/context/AuthContext'
-import React from 'react'
+// import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 export default function AuthOutlet() {
